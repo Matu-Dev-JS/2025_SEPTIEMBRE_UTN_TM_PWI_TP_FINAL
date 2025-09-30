c5d906953b4f22f249e5b81d7d85d778f6806171
matu.dev <mati.a.gimenez2002@gmail.com>
2025-09-30 11:45:55 -0300

    Agrego soporte para iconos

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -3,6 +3,8 @@ import { getAllContacts } from '../../services/contactService'
 import { Link } from 'react-router'
 import { ContactListContext } from '../../Context/ContactListContext'
 import WhatsAppSpinner from '../WhatsappSpinner/WhatsappSpinner'
+import ICONS from '../../constants/icons'
+
 
 const ContactList = () => {
     //useContext es un hook nativo de react que me permite consumir contextos
@@ -31,7 +33,7 @@ const ContactItem = (props) => {
     const contact = props.contact
     return (
         <Link to={'/contacto/' + contact.id} >
-            <h2>{contact.name}</h2>
+            <h2>{contact.name} <ICONS.Android className='android-icon' /> <ICONS.CodeIgniter/> <ICONS.Github/></h2>
             <span>En linea: {contact.is_connected ? 'Si' : 'No'}</span>
             <img src={contact.profile_img} width={'150px'} />
             <hr />
---
+++ src/constants/icons.jsx
@@ -0,0 +1,12 @@
+import { DiAndroid, DiCodeigniter } from "react-icons/di";
+import { FiGithub } from "react-icons/fi";
+
+
+
+const ICONS = {
+    Android: DiAndroid,
+    CodeIgniter: DiCodeigniter,
+    Github: FiGithub
+}
+
+export default ICONS
